d4cb0b407c62667ce3fa16e0c69eb418885e35f8
Valerio Santinelli <santinelli@altralogica.it>
2018-09-18 13:04:49 +0200

    More tests

--- src/Main.hx
+++ src/Main.hx
@@ -1,4 +1,6 @@
-class Main extends hxd.App {
+#if pippo
+
+class HeapsMain extends hxd.App {
     var g:h2d.Graphics;
     var bmp:h2d.Bitmap;
     var bmp2:h2d.Bitmap;
@@ -26,8 +28,63 @@ class Main extends hxd.App {
             g.drawRect(b.x, b.y, b.width, b.height);
         }
     }
+}
+
+class Main extends lime.app.Application {
+    var heapsApp: HeapsMain;
+
+    public function new() {
+        super();
+        heapsApp = new HeapsMain();
+    }
+
+	public override function render (context:lime.graphics.RenderContext):Void {
+    }
 
     static function main() {
-        new Main();
+        var app = new Main();
+        app.exec();
     }
 }
+#else
+
+class Main extends hxd.App {
+    var g:h2d.Graphics;
+    var bmp:h2d.Bitmap;
+    var bmp2:h2d.Bitmap;
+
+    override function init() {
+        trace('Main init');
+        g = new h2d.Graphics(s2d);
+
+        var tile = h2d.Tile.fromColor(0xFF0000, 100, 100);
+        bmp = new h2d.Bitmap(tile, s2d);
+        bmp.x = s2d.width * 0.5;
+        bmp.y = s2d.height * 0.5;
+
+        var tile2 = h2d.Tile.fromColor(0x00FF00, 100, 100);
+        bmp2 = new h2d.Bitmap(tile2, s2d);
+        bmp2.x = s2d.width * 0.5 - 150;
+        bmp2.y = s2d.height * 0.5;
+    }
+
+    override function update(dt:Float) {
+        trace('Main update');
+        g.clear();
+        bmp.rotation += 0.1;
+        if (bmp.getBounds().intersects(bmp2.getBounds())) {
+            var b = bmp.getBounds();
+            g.beginFill(0xFFFFFF, 0.5);
+            g.drawRect(b.x, b.y, b.width, b.height);
+        }
+    }
+
+    static function main() {
+        //hxd.Res.initEmbed();
+        trace("pre-instance");
+        new Main();
+        trace("post-instance");
+    }
+}
+
+#end
