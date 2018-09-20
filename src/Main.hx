5af6daeb4d9a34e477896de7790ee92f5df11cec
Valerio Santinelli <santinelli@altralogica.it>
2018-09-20 18:42:17 +0200

    We only draw one quad to make debugging simplet

--- src/Main.hx
+++ src/Main.hx
@@ -1,21 +1,23 @@
 class Main extends hxd.App {
     var g:h2d.Graphics;
     var bmp:h2d.Bitmap;
-    var bmp2:h2d.Bitmap;
+    //var bmp2:h2d.Bitmap;
 
     override function init() {
         trace('Main init');
         g = new h2d.Graphics(s2d);
-
+        
         var tile = h2d.Tile.fromColor(0xFF0000, 100, 100);
         bmp = new h2d.Bitmap(tile, s2d);
         bmp.x = s2d.width * 0.5;
         bmp.y = s2d.height * 0.5;
 
+        /*
         var tile2 = h2d.Tile.fromColor(0x00FF00, 100, 100);
         bmp2 = new h2d.Bitmap(tile2, s2d);
         bmp2.x = s2d.width * 0.5 - 150;
         bmp2.y = s2d.height * 0.5;
+        */
     }
 
     override function update(dt:Float) {
