c379593338aff0518117aff96bf9af8b1abba2e3
Valerio Santinelli <santinelli@altralogica.it>
2018-09-21 13:12:49 +0200

    Added back some moving objects

--- src/Main.hx
+++ src/Main.hx
@@ -1,7 +1,7 @@
 class Main extends hxd.App {
     var g:h2d.Graphics;
     var bmp:h2d.Bitmap;
-    //var bmp2:h2d.Bitmap;
+    var bmp2:h2d.Bitmap;
 
     override function init() {
         trace('Main init');
@@ -12,27 +12,23 @@ class Main extends hxd.App {
         bmp.x = s2d.width * 0.5;
         bmp.y = s2d.height * 0.5;
 
-        /*
         var tile2 = h2d.Tile.fromColor(0x00FF00, 100, 100);
         bmp2 = new h2d.Bitmap(tile2, s2d);
         bmp2.x = s2d.width * 0.5 - 150;
         bmp2.y = s2d.height * 0.5;
-        */
     }
 
     override function update(dt:Float) {
-        trace('Main update');
+        //trace('Main update');
         engine.backgroundColor = 0x404040;
         g.clear();
         
         bmp.rotation += 0.1;
-        /*
         if (bmp.getBounds().intersects(bmp2.getBounds())) {
             var b = bmp.getBounds();
             g.beginFill(0xFFFFFF, 0.5);
             g.drawRect(b.x, b.y, b.width, b.height);
         }
-        */
     }
 
     static function main() {
